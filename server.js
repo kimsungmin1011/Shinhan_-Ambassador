714ad15a40dd637b01647396ba9fc81b20ead638
kimsungmin1011 <tata153@naver.com>
2023-11-08 11:06:34 +0900

    80포트 변경

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
