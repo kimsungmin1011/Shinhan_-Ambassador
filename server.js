2bac4c1a691d07ebb59aec482000503e64d5af86
kimsungmin1011 <tata153@naver.com>
2023-11-08 11:13:11 +0900

    3000포트 재설정

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = 80;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
