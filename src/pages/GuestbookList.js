a3fe55eb87682c45e62f690cbf578f877ce1181e
kimsungmin1011 <tata153@naver.com>
2023-12-07 08:40:05 +0900

    팀 검색

--- src/pages/GuestbookList.js
+++ src/pages/GuestbookList.js
@@ -2,32 +2,52 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './style/Guestbook.css';
 
-
 function Guestbook() {
     const [entries, setEntries] = useState([]);
     const [name, setName] = useState('');
     const [content, setContent] = useState('');
+    const [team, setTeam] = useState('');
+    const [teamSetting, setTeamSetting] = useState('');
 
     useEffect(() => {
-        async function fetchEntries() {
-            const response = await axios.get('http://13.125.120.242:8080/guestbook');
-            setEntries(response.data.reverse()); // 최신순으로 가져오기
-        }
         fetchEntries();
-    }, []);
+    }, [teamSetting]); // Dependency array includes teamSetting
+
+    async function fetchEntries() {
+        try {
+            const response = await axios.get(`http://localhost:8080/guestbook/${teamSetting}`);
+            setEntries(response.data.reverse()); // 최신순으로 가져오기
+        } catch (error) {
+            console.error('Error fetching entries:', error);
+            // Handle error appropriately
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await axios.post('http://13.125.120.242:8080/guestbook', { name, content });
-        setName('');
-        setContent('');
-        // 전체 페이지를 다시 로드하지 않고 목록을 새로 고치기 위해 항목을 다시 가져옴
-        const response = await axios.get('http://13.125.120.242:8080/guestbook');
-        setEntries(response.data.reverse());
+        try {
+            await axios.post('http://localhost:8080/guestbook', { name, content, team });
+            setName('');
+            setContent('');
+            setTeam('');
+            fetchEntries(); // Refresh entries after submission
+        } catch (error) {
+            console.error('Error submitting entry:', error);
+            // Handle error appropriately
+        }
     }
+
+ 
 
     return (
         <div className="guestbook">
+            <input
+                value={teamSetting}
+                onChange={(e) => setTeamSetting(e.target.value)}
+                placeholder="팀별조회"
+                className="name-input"
+            />
+           
             <div className="entries">
                 {entries.map((entry, index) => (
                     <div key={index} className="entry">
@@ -35,6 +55,7 @@ function Guestbook() {
                     </div>
                 ))}
             </div>
+
             <form onSubmit={handleSubmit} className="form">
                 <input
                     value={name}
@@ -46,6 +67,12 @@ function Guestbook() {
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
                     placeholder="메시지를 입력하세요"
+                    className="content-textarea"
+                />
+                <textarea
+                    value={team}
+                    onChange={(e) => setTeam(e.target.value)}
+                    placeholder="팀을 입력하세요"
                     className="content-textarea"
                 />
                 <button type="submit" className="submit-btn">전송</button>
